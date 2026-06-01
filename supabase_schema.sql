6c62201119b6c6f94982690bedf34758e338fbe0
nabiliocp <rouijel.nabil.cp@gmail.com>
2026-06-01 22:52:11 +0200

    kkj

--- supabase_schema.sql
+++ supabase_schema.sql
@@ -24,6 +24,7 @@ create table public.profiles (
 -- Set up Row Level Security (RLS) for profiles
 alter table public.profiles enable row level security;
 create policy "Public profiles are viewable by everyone." on public.profiles for select using (true);
+create policy "Users can insert their own profile." on public.profiles for insert with check (auth.uid() = id);
 -- When users sign up via auth, the trigger will insert.
 create policy "Users can update own profile." on public.profiles for update using (auth.uid() = id);
 
